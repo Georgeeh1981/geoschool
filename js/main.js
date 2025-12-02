910687d522d820c0acc537c728f8fdfda4cdbfec
George Hernandez <georgeeh245@yahoo.es>
2025-12-01 19:21:31 -0500

    Initial GEOSCHOOL project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("GEOSCHOOL is running correctly!");
+
+const learnMoreBtn = document.getElementById("learnMoreBtn");
+
+if (learnMoreBtn) {
+  learnMoreBtn.addEventListener("click", () => {
+    alert("Soon this will be your full GEOSCHOOL platform in the cloud. 🚀");
+  });
+}
